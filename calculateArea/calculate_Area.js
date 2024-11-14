8aabbe67075a183b72a942ca65ef9b5179331e67
Vinod Reddy <vinodreddybheemanagouda@gmail.com>
2024-11-14 06:14:02 -0500

    Calculate Area

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,10 +1,12 @@
-let length;
-let width;
+let Grocery_1;
+let Grocery_2;
+let Grocery_3;
 
-function calculateArea() {
- length = parseFloat(document.getElementById('length').value);
- width = parseFloat(document.getElementById('width').value);
+function groceryTracker() {
+    Grocery_1 = parseFloat(document.getElementById('Grocery_1').value);
+    Grocery_2 = parseFloat(document.getElementById('Grocery_2').value);
+    Grocery_3 = parseFloat(document.getElementById('Grocery_3').value);
 
- let area = length * width;
- document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+ let total = Grocery_1+Grocery_2+Grocery_3;
+ document.getElementById('result').innerText = `The area of the rectangle is: $ ${total}`;
 }
